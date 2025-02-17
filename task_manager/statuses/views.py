f179819d51d292b5463b316a99b197f150d1313a
AlloKuz <a.o.kuzmin.rus@gmail.com>
2025-02-17 17:35:02 +0300

    Add unique constrain to Status:name field

--- task_manager/statuses/views.py
+++ task_manager/statuses/views.py
@@ -2,12 +2,14 @@ from django.shortcuts import render, redirect, reverse
 from django.views import View
 from django.contrib import messages
 from django.utils.translation import gettext as _
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 from statuses.models import Status
 from statuses.forms import StatusForm
 
 
-class IndexView(View):
+class IndexView(LoginRequiredMixin, View):
+    login_url = "/login/"
 
     def get(self, request, *args, **kwargs):
         statuses = Status.objects.all()
@@ -15,7 +17,8 @@ class IndexView(View):
                       context={"statuses": statuses})
 
 
-class StatusCreateView(View):
+class StatusCreateView(LoginRequiredMixin, View):
+    login_url = "/login/"
 
     def get(self, request, *args, **kwargs):
         form = StatusForm()
@@ -28,6 +31,38 @@ class StatusCreateView(View):
             form.save()
             messages.info(request, _("Статус успешно создан"))
             return redirect(reverse("statuses"))
-        messages.error(request, _("Неправильное имя статуса"))
         return render(request, "statuses/create.html",
                       {"form": form})
+
+
+class StatusUpdateView(LoginRequiredMixin, View):
+
+    def get(self, request, id, *args, **kwargs):
+        status = Status.objects.get(id=id)
+        form = StatusForm(instance=status)
+        return render(request, "statuses/update.html",
+                      context={"form": form, "status_id": id})
+
+    def post(self, request, id, *args, **kwargs):
+        status = Status.objects.get(id=id)
+        form = StatusForm(request.POST, instance=status)
+        if form.is_valid():
+            form.save()
+            messages.info(request, _("Статус успешно изменен"))
+            return redirect(reverse("statuses"))
+        return render(request, "statuses/update.html",
+                      {"form": form, "status_id": id})
+
+
+class StatusDeleteView(LoginRequiredMixin, View):
+    def get(self, request, id, *args, **kwargs):
+        status = Status.objects.get(id=id)
+        return render(request, "statuses/delete.html",
+                      context={"status_id": id, "status": status})
+
+    def post(self, request, id, *args, **kwargs):
+        status = Status.objects.get(id=id)
+        if status:
+            status.delete()
+        messages.info(request, _("Статус успешно удален"))
+        return redirect(reverse("statuses"))
