d03b58484fb64b397ff39e8819771eeac6ff1c21
AlloKuz <a.o.kuzmin.rus@gmail.com>
2025-02-17 16:40:32 +0300

    some chng

---
+++ task_manager/statuses/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+
+from statuses.views import IndexView, StatusCreateView
+
+urlpatterns = [
+    path('', IndexView.as_view(), name="statuses"),
+    path('create/', StatusCreateView.as_view(), name="statuses_create"),
+    ]
