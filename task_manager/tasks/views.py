e444f37ac2e2fe3a486255610545e0a4203ebb9a
AlloKuz <a.o.kuzmin.rus@gmail.com>
2025-02-24 13:22:49 +0300

    fix after review (6)

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -13,6 +13,7 @@ from django.views.generic import (
     UpdateView,
 )
 
+from django_filters.views import FilterView
 from task_manager.tasks.filters import TasksFilter
 from task_manager.tasks.models import Task
 from task_manager.users.mixins import LoginRequiredWithMessageMixin
@@ -20,16 +21,10 @@ from task_manager.users.mixins import LoginRequiredWithMessageMixin
 logger = logging.getLogger(__name__)
 
 
-class TaskListView(LoginRequiredWithMessageMixin, ListView):
+class TaskListView(LoginRequiredWithMessageMixin, FilterView):
     model = Task
-
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["filters"] = TasksFilter(self.request.GET,
-                                         queryset=Task.objects.all(),
-                                         request=self.request)
-        return context
-
+    filterset_class = TasksFilter
+    template_name = 'task_list.html'
 
 class TaskCreateView(LoginRequiredWithMessageMixin,
                      SuccessMessageMixin, CreateView):
