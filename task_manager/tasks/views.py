9b0a069a54bc3c85812412cbfc1c4809e1b2f9ab
AlloKuz <a.o.kuzmin.rus@gmail.com>
2025-03-05 15:24:56 +0300

    fix after 2 review (3)

--- task_manager/tasks/views.py
+++ task_manager/tasks/views.py
@@ -94,7 +94,4 @@ class TaskDeleteView(LoginRequiredWithMessageMixin,
 
     def post(self, request, *args, **kwargs):
         data = super().post(request, *args, **kwargs)
-        if self.object.author_id != request.user.id:
-            messages.error(request, _("Task can be deleted only by author"))
-            return redirect(reverse_lazy('tasks'))
         return data
